b971a0888b7ee6b5373bbb01ff207bd03e3dfac0
Kiran Bhattaram <kiranb@mit.edu>
2012-04-14 14:26:16 -0400

    added the start page

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ ConcertBOS::Application.routes.draw do
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
+  match 'start' => 'welcome#start'
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
