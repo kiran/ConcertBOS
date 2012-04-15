95fb38c4300f7cfd45ac41d1e38d8324bc5f357f
Kiran <kiran@anythingsocial.com>
2012-04-15 18:57:52 -0400

    ability to delete in concert drag

--- app/assets/javascripts/concertdrag.js
+++ app/assets/javascripts/concertdrag.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('#concerts .btn').draggable({revert: "invalid", // when not dropped, the item will revert back to its initial position
+  $('#concerts .btn').draggable({//revert: "invalid", // when not dropped, the item will revert back to its initial position
   			helper: "clone",
   			cursor: "move"});
 			
@@ -11,20 +11,46 @@ $(document).ready(function() {
   	}
   });
   
+  $('#concerts-list').on('click', function(e){
+    e.preventDefault();
+    window.e = e;
+    deletePreference($(e.target));
+  });
+  
   function addPreference (item) {
+    //item.draggable('disable');
     // get the concert id
     var id = /\d+/.exec(item.attr('href'))[0];
     // set the user id to 1
     var info = JSON.stringify({'concert_id':id,'user_id':1});
     // add to the list on success
-    var success = function() {
-      item.draggable('disable');
+    var success = function(e) {
       var list = $('#concerts-list');
-      item.clone().appendTo(list).fadeIn();
+      var marker = item.clone();
+      marker.attr('id', e.id);
+      marker.text(item.parent().siblings().text());
+      marker.appendTo(list).fadeIn();
       list.append('<br>');
     }
     // send to the server: {concert_id: xx, user_id: 1}, method = POST, url = concertsusers/create
     var options = {url : '/concertsusers', type: 'POST', // URL and method to call 
+      contentType : 'application/json', dataType: 'json', // send and receive data from the server as JSON}
+      data: info, success:success};
+    
+    $.ajax(options);
+  }
+  
+  function deletePreference (item) {
+    // get the concert id
+    var id = item.attr('id');
+    // set the user id to 1
+    var info = JSON.stringify({'concert_id':id,'user_id':1});
+    // add to the list on success
+    var success = function() {
+      var marker = item.fadeOut();
+    }
+    // send to the server: {concert_id: xx, user_id: 1}, method = POST, url = concertsusers/create
+    var options = {url : '/concertsusers/'+id, type: 'DELETE', // URL and method to call 
       contentType : 'application/json', dataType: 'json', // send and receive data from the server as JSON}
       data: info, success:success};
     
