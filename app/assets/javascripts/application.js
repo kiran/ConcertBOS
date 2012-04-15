2a32e9f2ea7c598a1296fe410935963e0215d0d4
Kiran <kiran@anythingsocial.com>
2012-04-15 02:10:21 -0400

    whoops.  fixed a backbone dependancy.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,9 +14,5 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require underscore
-//= require backbone
-//= require backbone_rails_sync
-//= require backbone_datalink
-//= require backbone/concert_bos
 //= require_tree .
 //= require jquery-ui
