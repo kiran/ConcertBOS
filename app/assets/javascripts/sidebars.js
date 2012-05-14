0d85988b62b84e1aac0a631f6bc1aec1560d0fea
Kiran Bhattaram <kiranb@mit.edu>
2012-05-14 16:06:24 -0400

    calendar now age updates

--- app/assets/javascripts/sidebars.js
+++ app/assets/javascripts/sidebars.js
@@ -5,6 +5,22 @@ $(function() {
     {
 			window.paint();
 			window.fpaint();
+			fixCalendar();
+    }
+
+    function fixCalendar(event) {
+        eighteen = ($('#eighteen').attr('checked') == 'checked'); //under 18
+        twentyOne = ($('#twentyone').attr('checked') == 'checked'); //under 21
+
+        if (eighteen) {
+            $('.over18').css('background-color', 'lightgray');
+            $('.over21').css('background-color', 'lightgray');
+        } else if (twentyOne) {
+            $('.over18').css('background-color', '');
+            $('.over21').css('background-color', 'lightgray');
+        } else {
+            $('.over18, .over21').css('background-color','');
+        }
     }
 });
 
