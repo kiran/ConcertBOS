e09ffd78b2f3c6f2731b6be62f5d617b75cb17c5
Kiran Bhattaram <kiranb@mit.edu>
2012-05-14 05:57:07 -0400

    can page through months

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ group :assets do
 
   gem 'uglifier', '>= 1.0.3'
   gem 'twitter-bootstrap-rails'
+  gem 'font-awesome-rails'
 end
 
 gem 'jquery-rails'
--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,4 +1,9 @@
 class CalendarController < ConcertsController
 
+    def index
+        @concerts = Concert.all
+        @date = params[:month] ? Date.parse(params[:month]) : Date.today
+    end
+
 end
 
